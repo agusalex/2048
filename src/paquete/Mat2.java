a1801295fab0bdb4fbb30cc75bde01b730f036e9
agusalexander <ripazha1>
2016-08-25 20:00:56 -0300

    Agrego funcion getDirectionParameters Mat2 = Terminada?

--- src/paquete/Mat2.java
+++ src/paquete/Mat2.java
@@ -1,74 +1,57 @@
 package paquete;
 
-
-
-
-
 public class Mat2 {
 	public Integer[][] mat;
+	/**
+	 * Mueve la matriz
+	 * @param LEFT
+	 * @param RIGHT
+	 * @param DOWN
+	 * @param UP
+
+	 */
 	public enum direction{
 		LEFT,RIGHT,DOWN,UP
 	}
 
-	public  Mat2 duplicarmat(){
+	@Override
+	public boolean equals(Object otra1){
+		Mat2 otra=(Mat2) otra1;
 		
-		Mat2 otra= new Mat2(6);
-		
-		for(int x = 0; x < mat.length;x++){
-			for(int y = 0; y < mat.length;y++){
-				Integer actual = mat[x][y];
-				if(actual!=null){
-					otra.mat[x][y]=actual;
-				}
-			
-			
-			}}
-		return otra;
-	}
-	
-	
-	
-	public boolean esIgual(Mat2 otra){
-		
-		
-		if (otra==null){
+		if(otra==null){
 			return false;	
 		}
-		/*if(this.mat.length!=otra.mat.length){
-			return false;
-		}*/
 		
-		for(int x = 0; x < mat.length;x++){
-			for(int y = 0; y < mat.length;y++){
-				Integer actual = mat[x][y];
-				Integer actual2 = otra.mat[x][y];
-		
-				
-				if(actual == null || actual2 == null){
-					if(!(actual == null && actual2 == null)){
-						return false;
-					}
+		else{
+			if(otra.mat==null||mat==null){
+				if(otra.mat==null&&mat==null){
+					return true;
 				}
 				else{
-				if(!(actual.equals(actual2))){
 					return false;
 				}
+				
+			}
+		}
+		if(otra.mat.length!=mat.length){
+		return false;
+		}
+		
+		
+		for(int x=0; x<otra.mat.length; x++){
+			for(int y=0; y<otra.mat.length; y++){
+				if(otra.mat[y][x]!=mat[y][x]){
+					return false;
 				}
 			}
 		}
 		
+		
+		
+		
+		
 		return true;
-		
-		
-		
-		
 	}
-	
-
-	
-	
-
-	
 	
 	public Mat2(int n){
 	
@@ -110,49 +93,24 @@ public class Mat2 {
 	 * esta funcion mueve de izq a derecha todos los elementos de la fila, despues de que estos fueron combinados
 	 * @param dir la direccion en la que se mueve la funcion
 	 * @param filaoCol es la fila o columna en la que se opera
-	 * @return
 	 */
-	
 	public void Move(direction dir, int filaoCol){
-		int y,x,from,to,indexDir,indexAux = 0;
+		int from,to,indexDir,x,y,indexAux;
 		
-		boolean xaxis=false;
-		boolean yaxis=false;
+		boolean xaxis,yaxis;
+		
+		Object[] parameters=getDirectionParameters(dir,filaoCol);
+		
+		from=(int) parameters[0];
+		to=(int) parameters[1];
+		indexDir=(int) parameters[2];
+		x=(int) parameters[3];
+		y=(int) parameters[4];
+		xaxis=(boolean) parameters[5];
+		yaxis=(boolean) parameters[6];
+		////
 		Integer [] aux = new Integer [mat.length];
 		Integer Current = null;
-		
-		if (dir.equals(direction.LEFT)){
-			from=0;
-			to=mat.length;
-			indexDir=1;
-			y=filaoCol;
-			x=from;
-			xaxis=true;
-		}
-		else if (dir.equals(direction.RIGHT)){
-			from=mat.length-1;
-			to=-1;
-			indexDir=-1;
-			y=filaoCol;
-			x=from;
-			xaxis=true;
-		}
-		else if (dir.equals(direction.UP)){
-			from=0;
-			to=mat.length;
-			indexDir=1;
-			x=filaoCol;
-			y=from;
-			yaxis=true;
-		}
-		else{
-			from=mat.length-1;
-			to=-1;
-			indexDir=-1;
-			x=filaoCol;
-			y=from;
-			yaxis=true;
-		}
 		
 		indexAux=from;
 		
@@ -187,24 +145,28 @@ public class Mat2 {
 		
 	}
 	
-	public void Shift(direction dir){
+	/**
+	 * Mueve la matrix y conbina todas  sus filas en la direccion indicada
+	 * @param Direction la direccion en la que se mueve la funcion
+	 * @param filaoCol es la fila o columna en la que se opera
+
+	 */
+	public void Shift(direction Direction){
 		for(int x=0;x<mat.length;x++){
-				combineCells(dir,x);
-				Move(dir,x);
+				combineCells(Direction,x);
+				Move(Direction,x);
 		}
 		
 	}
-	
+
+
 	/**
-	 * combina las celdas que tengan mismo numero, revisa de izquierda a derecha, buscando un numero identico a la celda actual
-	 * una vez que los combina, pasa a buscar otra operacion
+	 * Devuelve los parametros para recorrer el arreglo en la direccion y orientacion correctas
 	 * @param dir la direccion en la que se mueve
 	 * @param filaoCol la fila o columna sobre la que se opera
-	 * @return booleano este indica si se realizo alguna combinacion para poder pasar a reubicar los resultados
+	 * @return Retorna un arreglo de objetos con los parametros, tanto int como boolean 
 	 */
-	
-	
-	public Object[] directionParameters(direction dir,int filaoCol){
+	private Object[] getDirectionParameters(direction dir,int filaoCol){
 		int y,x,from,to,indexDir;
 		boolean xaxis=false;
 		boolean yaxis=false;
@@ -249,51 +211,26 @@ public class Mat2 {
 	}
 	
 	
-	
+	/**
+	 * combina las celdas que tengan mismo numero, revisa de izquierda a derecha, buscando un numero identico a la celda actual
+	 * una vez que los combina, pasa a buscar otra operacion
+	 * @param dir la direccion en la que se mueve
+	 * @param filaoCol la fila o columna sobre la que se opera
+	 */
 	public void combineCells(direction dir,int filaoCol ){
 		int from,to,indexDir,x,y;
 		boolean xaxis,yaxis;
-		Object[] parameters=directionParameters(dir,filaoCol);
-		xaxis=false;
-		yaxis=false;
 		
+		Object[] parameters=getDirectionParameters(dir,filaoCol);
 		
-		
-		
-		
-		if (dir.equals(direction.LEFT)){
-			from=0;
-			to=mat.length;
-			indexDir=1;
-			y=filaoCol;
-			x=from;
-			xaxis=true;
-		}
-		else if (dir.equals(direction.RIGHT)){
-			from=mat.length-1;
-			to=-1;
-			indexDir=-1;
-			y=filaoCol;
-			x=from;
-			xaxis=true;
-		}
-		else if (dir.equals(direction.UP)){
-			from=0;
-			to=mat.length;
-			indexDir=1;
-			x=filaoCol;
-			y=from;
-			yaxis=true;
-		}
-		else{
-			from=mat.length-1;
-			to=-1;
-			indexDir=-1;
-			x=filaoCol;
-			y=from;
-			yaxis=true;
-		}
-		
+		from=(int) parameters[0];
+		to=(int) parameters[1];
+		indexDir=(int) parameters[2];
+		x=(int) parameters[3];
+		y=(int) parameters[4];
+		xaxis=(boolean) parameters[5];
+		yaxis=(boolean) parameters[6];
+	
 		boolean checkIfEquals = true;         
 		int bkp = -1;					
 		Integer Current = null;   
